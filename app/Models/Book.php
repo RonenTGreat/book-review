a447727e8e77fbbb58857bb40f0a01c3a4929031
RonenTGreat <ronenh53@gmail.com>
2025-12-12 11:51:36 +0000

    Showing individual books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
+
 class Book extends Model
 {
     use HasFactory;
@@ -26,13 +27,14 @@ class Book extends Model
             ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighest(Builder $query, $from = null, $to = null)
+    public function scopeHighestRated(Builder $query, $from = null, $to = null)
     {
         return $query->withAvg(['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
-    public function scopeMinReviews(Builder $query, int $minReviews){
+    public function scopeMinReviews(Builder $query, int $minReviews)
+    {
         return $query->having('reviews_count', '>=', $minReviews);
     }
 
@@ -45,5 +47,32 @@ class Book extends Model
         } elseif ($from && $to) {
             $query->whereBetween('created_at', [$from, $to]);
         }
+    }
+
+    public function scopePopularLastMonth(Builder $query)
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+    public function scopePopularLast6Months(Builder $query)
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query)
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query)
+    {
+        return $query->highestRated(now()->subMonth(6), now())
+            ->popular(now()->subMonth(6), now())
+            ->minReviews(5);
     }
 }
